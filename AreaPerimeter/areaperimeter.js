9306acc6520845addf8e2c6614aabf8899905cb4
Brenda-Muli <mwengibrenda@gmail.com>
2024-06-05 10:04:37 +0300

    Area Perimeter Commit

---
+++ AreaPerimeter/areaperimeter.js
@@ -0,0 +1,8 @@
+let length = prompt('Enter length');
+let width = prompt('Enter width');
+
+const area = length * width;
+const perimeter = 2 * (length + width);
+
+console.log(area);
+console.log(perimeter);
